a44abdd3b04a003c732a54351e4ea96c80b3a927
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2018-02-17 15:18:47 -0500

    Started working on main scene

---
+++ Assets/Scripts/FallingObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FallingObject : MonoBehaviour, Actor {
+
+    public int fallingDamage = 5;
+    public int groundedDamage;
+
+    public void Init() { }
+    public void DoUpdate () { }
+    
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground") Behave();
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Death") Destroy(this.gameObject);
+    }
+
+    public abstract void Behave();
+}
