9bd4677521e9f9bb482de46883a26e520f86762f
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2018-02-17 16:57:39 -0500

    Kinda finished yellow dinosaurs

--- Assets/Scripts/Enemies/FallingObject.cs
+++ Assets/Scripts/Enemies/FallingObject.cs
@@ -7,7 +7,10 @@ public abstract class FallingObject : MonoBehaviour, Actor {
     public int fallingDamage = 5;
     public int groundedDamage;
 
+    protected float deathDelay;
     protected bool isGrounded;
+
+    void Awake() { deathDelay = 0.5f; }
 
     public void Init() { }
     public void DoUpdate () { }
@@ -17,7 +20,12 @@ public abstract class FallingObject : MonoBehaviour, Actor {
             isGrounded = true;
             Behave();
         }
-        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Death") Destroy(this.gameObject);
+        if (other.gameObject.tag == "Player") {
+            isGrounded = false;
+            Behave();
+        }
+        if (other.gameObject.tag == "Death") Destroy(this.gameObject);
+        if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Falling") Physics2D.IgnoreCollision(this.gameObject.GetComponent<Collider2D>(), other.gameObject.GetComponent<Collider2D>(), true);
     }
 
     public abstract void Behave();
