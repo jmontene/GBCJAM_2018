5f43391d15e6ad9b3b78fa4db759c6e1bca21cf9
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2018-02-17 15:33:11 -0500

    Done with the green dinos

---
+++ Assets/Scripts/Enemies/GreenDino.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenDino : FallingObject {
+
+    private float deathDelay;
+    private Animator anim;
+
+    void Awake() {
+        deathDelay = 0.5f;
+        anim = GetComponent<Animator>();
+    }
+
+    public override void Behave() {
+        this.gameObject.tag = "Enemy";
+        if (anim != null) anim.SetTrigger("wasHurt");
+        Destroy(this.gameObject, deathDelay);
+    }
+}
---
+++ Assets/Scripts/Enemies/YellowDino.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowDino : FallingObject {
+
+    private float deathDelay;
+    private Animator anim;
+
+    void Awake() {
+        deathDelay = 0.5f;
+        anim = GetComponent<Animator>();
+    }
+
+    public override void Behave() {
+        this.gameObject.tag = "Enemy";
+        if (anim != null) anim.SetTrigger("wasHurt");
+    }
+}
