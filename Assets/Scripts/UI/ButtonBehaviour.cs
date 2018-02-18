a4a58ab150b20019854082d81adf25208c0088bd
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2018-02-17 20:08:53 -0500

    latest changes to menu screen

---
+++ Assets/Scripts/UI/ButtonBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviour : MonoBehaviour {
+
+    [SerializeField] private int sceneToLoad;
+
+    public void DoSceneChange() { SceneManager.LoadScene(sceneToLoad); }
+}
