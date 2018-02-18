5c33faa822f8fdbe5fdb5e89a203065b7eba29a7
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2018-02-17 20:36:19 -0500

    Finished intro scene

--- Assets/Scripts/UI/ButtonBehaviour.cs
+++ Assets/Scripts/UI/ButtonBehaviour.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems; // for Mouse Event handlers on Canvas
 
-public class ButtonBehaviour : MonoBehaviour {
+public class ButtonBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
     [SerializeField] private int sceneToLoad;
 
+    private GameObject sprite;
+
+    void Awake() { sprite = transform.Find("IconSelect").gameObject; }
+
+    void Start() { sprite.SetActive(false); }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData d) { if (sprite != null) sprite.SetActive(true); }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData d) { if (sprite != null) sprite.SetActive(false); }
+
     public void DoSceneChange() { SceneManager.LoadScene(sceneToLoad); }
+
+    public void QuitApp() { Application.Quit(); }
 }
